e53e220812c6fb667c1a9bf6ad6528a461371c51
gabrielver <verronet.g@gmail.com>
2022-04-16 14:42:29 +0200

    last commit

---
+++ Ultimate Animations Starter/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYv-9OF2vhfw2jSgYwRYoWt_fKhXsBPG0",
+  authDomain: "animation-gsap.firebaseapp.com",
+  projectId: "animation-gsap",
+  storageBucket: "animation-gsap.appspot.com",
+  messagingSenderId: "234678175865",
+  appId: "1:234678175865:web:28a39383e861ed6df36ca8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
